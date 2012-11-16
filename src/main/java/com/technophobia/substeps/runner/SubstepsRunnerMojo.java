a28f56504850465c4552797cae52b52685bb389d
Ian Moore <imoore@technophobia.com>
2012-11-16 17:01:03 +0000

    jmx updates

--- src/main/java/com/technophobia/substeps/runner/SubstepsRunnerMojo.java
+++ src/main/java/com/technophobia/substeps/runner/SubstepsRunnerMojo.java
@@ -27,6 +27,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import junit.framework.Assert;
 
@@ -53,8 +54,6 @@ import com.technopobia.substeps.jmx.SubstepsJMXClient;
  * @configurator include-project-dependencies
  */
 public class SubstepsRunnerMojo extends AbstractMojo {
-
-
 
     /**
      * Location of the file.
@@ -91,143 +90,177 @@ public class SubstepsRunnerMojo extends AbstractMojo {
      */
     private final ExecutionReportBuilder executionReportBuilder = null;
 
+    /**
+     * @parameter default-value="9999"
+     * @required
+     */
+    private int jmxPort;
+
+    /**
+     * @parameter
+     */
+    private final String vmArgs = null;
+
     private Process forkedJVMProcess = null;
 
-    
+
     public void execute() throws MojoExecutionException, MojoFailureException {
 
-    	startMBeanJVM();
-    	
-    	
+        startMBeanJVM();
+
         final BuildFailureManager buildFailureManager = new BuildFailureManager();
 
-    	// TODO - now fire up the JMX Connection, run the tests in the spawned VM
+        // TODO - now fire up the JMX Connection, run the tests in the spawned
+        // VM
 
-        executeInternal(buildFailureManager, executionConfigs);
-        
-		try
-		{
-			System.out.println("waiting for forked process to return");
-			
-			if (forkedJVMProcess != null)
-			{
-				forkedJVMProcess.waitFor();
-			}
-		}
-		catch (final InterruptedException e)
-		{
-			// TODO
-			e.printStackTrace();
-		}
-		System.out.println("done");
+        executeInternal(buildFailureManager, this.executionConfigs);
+
+        try {
+            System.out.println("waiting for forked process to return");
+
+            if (this.forkedJVMProcess != null) {
+                this.forkedJVMProcess.waitFor();
+            }
+        } catch (final InterruptedException e) {
+            // TODO
+            e.printStackTrace();
+        }
+        System.out.println("done");
     }
-	/**
-	 * 
-	 */
-	private void startMBeanJVM()
-	{
-		// launch the jvm process that will contain the Substeps MBean Server
-    	// build up the class path based on this projects classpath
-    	
-		final CountDownLatch processStarted = new CountDownLatch(1);
-		
-		try {
-    		// strings
-			final List testClassPathElements = project.getTestClasspathElements();
-			final StringBuilder cpBuf = new StringBuilder();
-			
-			boolean first = true;
-			for (final Object element : testClassPathElements){
-				
-				if (!first){
-					cpBuf.append(File.pathSeparator);
-				}
-				cpBuf.append((String)element);
-				first = false;
-			}
-			
-			
-			final String[] command = new String[]{"java", 
-					"-Dfile.encoding=UTF-8",
-					"-Dcom.sun.management.jmxremote.port=9999",
-					"-Dcom.sun.management.jmxremote.authenticate=false",
-					"-Dcom.sun.management.jmxremote.ssl=false",
-					"-Djava.rmi.server.hostname=localhost",
-					"-classpath",
-					cpBuf.toString(),
-					"com.technopobia.substeps.jmx.SubstepsJMXServer"};
-			
-			
-			final ProcessBuilder processBuilder = new ProcessBuilder(command);
-	 
-		    processBuilder.redirectErrorStream(true);
-		    
-			try {
-				forkedJVMProcess = processBuilder.start();
-				
-				final InputStream stderr = forkedJVMProcess.getInputStream();
-				final InputStreamReader isr = new InputStreamReader(stderr);
-				final BufferedReader br = new BufferedReader(isr);
-				
-				final Thread t = new Thread(new Runnable(){
 
-					public void run(){
-						String line = null;
 
-						try {
-							while ((line = br.readLine()) != null) {
-								if (line.compareToIgnoreCase("awaiting the shutdown notification...")==0){
-									System.out.println("mbean server process started");
-									processStarted.countDown();
-								}
-								System.out.println(" *\t\t" + line);	
-							}
-						}
-						catch (final IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}});
-				t.start();
-			}
-			catch (final IOException e){
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		catch (final DependencyResolutionRequiredException e){
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			System.out.println("waiting for process to start...");
-			processStarted.await();
-			System.out.println("process started");
-		}
-		catch (final InterruptedException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-    
-    private void executeInternal(final BuildFailureManager buildFailureManager, 
-    		final List<ExecutionConfig> executionConfigList) throws MojoFailureException {
+    /**
+     * @throws MojoExecutionException
+     * 
+     */
+    private void startMBeanJVM() throws MojoExecutionException {
+        // launch the jvm process that will contain the Substeps MBean Server
+        // build up the class path based on this projects classpath
+
+        final CountDownLatch processStarted = new CountDownLatch(1);
+        final AtomicBoolean processStartedOk = new AtomicBoolean(false);
+
+        try {
+            // strings
+            final List testClassPathElements = this.project
+                    .getTestClasspathElements();
+            final StringBuilder cpBuf = new StringBuilder();
+
+            boolean first = true;
+            for (final Object element : testClassPathElements) {
+
+                if (!first) {
+                    cpBuf.append(File.pathSeparator);
+                }
+                cpBuf.append((String) element);
+                first = false;
+            }
+
+            final List<String> command = new ArrayList<String>();
+            command.add("java");
+            command.add("-Dfile.encoding=UTF-8");
+            command.add("-Dcom.sun.management.jmxremote.port=" + this.jmxPort);
+            command.add("-Dcom.sun.management.jmxremote.authenticate=false");
+            command.add("-Dcom.sun.management.jmxremote.ssl=false");
+            command.add("-Djava.rmi.server.hostname=localhost");
+
+            if (this.vmArgs != null && !this.vmArgs.isEmpty()) {
+                final String[] args = this.vmArgs.split(" ");
+                for (final String arg : args) {
+                    command.add(arg);
+                }
+            }
+
+            command.add("-classpath");
+            command.add(cpBuf.toString());
+            command.add("com.technopobia.substeps.jmx.SubstepsJMXServer");
+
+            final ProcessBuilder processBuilder = new ProcessBuilder(command);
+
+            processBuilder.redirectErrorStream(true);
+
+            try {
+                this.forkedJVMProcess = processBuilder.start();
+
+                final InputStream stderr = this.forkedJVMProcess
+                        .getInputStream();
+                final InputStreamReader isr = new InputStreamReader(stderr);
+                final BufferedReader br = new BufferedReader(isr);
+
+                final Thread t = new Thread(new Runnable() {
+
+                    public void run() {
+                        String line = null;
+
+                        try {
+                            while ((line = br.readLine()) != null) {
+                                if (line.compareToIgnoreCase("awaiting the shutdown notification...") == 0) {
+                                    System.out
+                                            .println("mbean server process started");
+                                    processStartedOk.set(true);
+                                    processStarted.countDown();
+
+                                }
+                                System.out.println(" *\t\t" + line);
+                            }
+                        } catch (final IOException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        } finally {
+                            if (processStarted.getCount() > 0) {
+                                System.out
+                                        .println("spawned process didn't start fully, no further output, an error is assumed and the process will terminate");
+                                processStarted.countDown();
+                            }
+                        }
+                    }
+                });
+                t.start();
+            } catch (final IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } catch (final DependencyResolutionRequiredException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            System.out.println("waiting for process to start...");
+            processStarted.await();
+
+            if (!processStartedOk.get()) {
+                throw new MojoExecutionException("Unable to launch VM process");
+            }
+
+            System.out.println("process started");
+        } catch (final InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
+    private void executeInternal(final BuildFailureManager buildFailureManager,
+            final List<ExecutionConfig> executionConfigList)
+            throws MojoFailureException {
 
         final ReportData data = new ReportData();
 
-        Assert.assertNotNull("executionConfigs cannot be null", executionConfigList);
-        Assert.assertFalse("executionConfigs can't be empty", executionConfigList.isEmpty());
+        Assert.assertNotNull("executionConfigs cannot be null",
+                executionConfigList);
+        Assert.assertFalse("executionConfigs can't be empty",
+                executionConfigList.isEmpty());
 
         final SubstepsJMXClient client = new SubstepsJMXClient();
 
-        client.init();
-        
+        client.init(this.jmxPort);
+
         for (final ExecutionConfig executionConfig : executionConfigList) {
 
             final List<SubstepExecutionFailure> failures = new ArrayList<SubstepExecutionFailure>();
-        	
-        	final ExecutionNode rootNode = runExecutionConfig(executionConfig, failures,client);
+
+            final ExecutionNode rootNode = runExecutionConfig(executionConfig,
+                    failures, client);
 
             if (executionConfig.getDescription() != null) {
 
@@ -238,48 +271,46 @@ public class SubstepsRunnerMojo extends AbstractMojo {
 
             buildFailureManager.sortFailures(failures);
         }
-        
+
         client.shutdown();
 
-        
-        
-        if (executionReportBuilder != null) {
-            executionReportBuilder.buildReport(data);
+        if (this.executionReportBuilder != null) {
+            this.executionReportBuilder.buildReport(data);
         }
 
-        
-        if(buildFailureManager.testSuiteFailed()){
-        	
-            throw new MojoFailureException("Substep Execution failed:\n" + buildFailureManager.getBuildFailureInfo());
+        if (buildFailureManager.testSuiteFailed()) {
 
-        }
-        else if (!buildFailureManager.testSuiteCompletelyPassed()){
-        	// print out the failure string (but won't include any failures)
-        	getLog().info(buildFailureManager.getBuildFailureInfo());
+            throw new MojoFailureException("Substep Execution failed:\n"
+                    + buildFailureManager.getBuildFailureInfo());
+
+        } else if (!buildFailureManager.testSuiteCompletelyPassed()) {
+            // print out the failure string (but won't include any failures)
+            getLog().info(buildFailureManager.getBuildFailureInfo());
         }
         // else - we're all good
-        
-    }
-    
-    private ExecutionNode runExecutionConfig(final ExecutionConfig theConfig, 
-			final List<SubstepExecutionFailure> failures, final SubstepsJMXClient client  ) {
-	
-    	
-        final ExecutionNode rootNode = client.prepareExceutionConfig(theConfig);
-        //client.runExecutionConfig(theConfig);
 
-    	
-//	    final ExecutionNodeRunner runner = new ExecutionNodeRunner();
-	
-	    //final ExecutionNode rootNode = runner.prepareExecutionConfig(theConfig);
-	
-	    //final List<SubstepExecutionFailure> localFailures = runner.run();
-	    
+    }
+
+
+    private ExecutionNode runExecutionConfig(final ExecutionConfig theConfig,
+            final List<SubstepExecutionFailure> failures,
+            final SubstepsJMXClient client) {
+
+        final ExecutionNode rootNode = client.prepareExceutionConfig(theConfig);
+        // client.runExecutionConfig(theConfig);
+
+        // final ExecutionNodeRunner runner = new ExecutionNodeRunner();
+
+        // final ExecutionNode rootNode =
+        // runner.prepareExecutionConfig(theConfig);
+
+        // final List<SubstepExecutionFailure> localFailures = runner.run();
+
         final List<SubstepExecutionFailure> localFailures = client.run();
-        
-	    failures.addAll(localFailures);
-	    
-	    return rootNode;
+
+        failures.addAll(localFailures);
+
+        return rootNode;
     }
 
 }
